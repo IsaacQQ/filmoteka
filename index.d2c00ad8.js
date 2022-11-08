d76d389783d39bbbb79c637fe4a86515ecd1eb9e
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-11-08 19:33:34 +0000

    Deploying to gh-pages from @ AlexAmanitas/filmoteka-team-11@a887057dc9cc185ba7ffab61e6dc8601512a3687 🚀

---
+++ index.d2c00ad8.js
@@ -0,0 +1,2 @@
+!function(){var e={closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.closeModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.d2c00ad8.js.map
